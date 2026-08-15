155c8aafb220916cdfcd4ffcb81313b3ba23e2c1
PieroJF <drsmugin@gmail.com>
2026-08-15 13:13:54 +0100

    test: extend a11y and visual coverage to new routes

--- tests/e2e/a11y.spec.ts
+++ tests/e2e/a11y.spec.ts
@@ -1,7 +1,20 @@
 import AxeBuilder from '@axe-core/playwright';
 import { expect, test } from '@playwright/test';
 
-const paths = ['/', '/projects', '/blog', '/blog/qarm-fruit-sorting', '/blog/fred-factory-station-design', '/cv', '/404'];
+const paths = [
+  '/',
+  '/projects',
+  '/blog',
+  '/blog/qarm-fruit-sorting',
+  '/blog/fred-factory-station-design',
+  '/blog/argos-can-reverse-engineering',
+  '/blog/solidworks-drawing-automation',
+  '/blog/lattice-planning-comparison',
+  '/blog/visual-regression-shared-component',
+  '/blog/qarm-camera-calibration-debugging',
+  '/cv',
+  '/404',
+];
 
 for (const path of paths) {
   test(`axe: no WCAG A/AA violations on ${path}`, async ({ page }) => {
